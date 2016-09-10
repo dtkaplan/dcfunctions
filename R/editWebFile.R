d144722130273f4adf496305121f0c1f5080350b
dtkaplan <dtkaplan@gmail.com>
2016-09-10 08:56:14 -0700

    Streamlined package and removed any use of RCurl or related functions.

---
+++ R/editWebFile.R
@@ -0,0 +1,29 @@
+#' Display a file from the web in the editor
+#'
+#' Reads a text file from the web and opens the contents in an editor.
+#' This is useful, for instance, for sharing assignment templates with students.
+#' You MUST "save as" the editor buffer in your own folder
+#' if you want to keep it.
+#'
+#' @return The temporary file name, invisibly.
+#' @param url URL locating the file on the web
+#' @examples \dontrun{
+#'    editWebFile("www.mosaic-web.org/go/datasets/angle.R")
+#' }
+#' @export
+editWebFile <- function(url){
+
+  fname <- basename(url)
+  partsOfName <- stringr::str_match_all("our_file_today.html",
+                                        "(^.+)(\\.(Rmd|r|R|md|tex|.{1,4})$)")
+  filename <- unlist(partsOfName)[2]
+  extension <- unlist(partsOfName)[3]
+  tmpname <- tempfile(pattern=paste0(filename,"---"), fileext=extension)
+  download.file(url, dest = tmpname)
+  file.edit(tmpname, title = filename)
+  message("Remember to save as ",
+          paste0(filename,extension),
+          " in your own folder. Until you do, it's just temporary.")
+  invisible(tmpname)
+}
+
---
+++ R/readStockPrices.R
@@ -0,0 +1,66 @@
+#' Read stock and dividends from yahoo.finance
+#'
+#' This is a convenience function for exercises in the Data Computing book. It reads stock prices
+#' and dividends over a specified time interval.
+#'
+#' @details This function relies on the finance.yahoo.com API as it exists in early 2015.
+#'
+#' @param symbols a single character string or a vector of character strings specifying
+#' a stock listed on yahoo.finance.
+#' @param what whether to read prices or dividends.  For prices, use one of \code{"daily"}, \code{"weekly"}, \code{"monthly"}.
+#' For dividends, use \code{"dividends"}.
+#' @param startYear the lower end of the time interval for which to download data
+#' @param endYear the upper end of the time interval
+#'
+#' @return A data frame.  One column in the data frame lists the stock symbol.  This
+#' is useful when reading data for more than one stock.
+#'
+#' @examples
+#' \dontrun{CarStocks <- readStockPrices("F", "monthly", 1995, 2012)}
+#' @export
+read_stock_prices <- function(symbols='F',
+                            what=c("prices","daily","weekly", "monthly", "dividends"),
+                            start_year=1972,end_year=2015) {
+
+  if (! require(dplyr)) stop("Must install dplyr package.")
+  what <- match.arg(what)
+  yahooCode <- switch(what,
+                      prices = ,
+                      daily = "d",
+                      weekly = "w",
+                      monthly = "m",
+                      dividends = "v",
+                      "unknown")
+
+  stockURL <- "http://real-chart.finance.yahoo.com/table.csv?s=%s&a=05&b=1&c=%d&d=01&e=25&f=%d&g=%s&ignore=.csv"
+
+  output <- NULL # for collecting output
+
+  for (symbol in symbols) {
+    thisURL <- sprintf(stockURL, symbol, start_year, end_year, yahooCode)
+    tmpname <- tempfile("stocks", fileext = "csv")
+    con <- try(suppressWarnings(download.file(thisURL, dest = tmpname)),
+               silent = TRUE)
+    if (inherits(con, what = "try-error"))
+      stop(paste("Symbol", symbol, "not found in years",
+                 start_year, "to", end_year, "on Yahoo finance."))
+    res <- read.csv(tmpname, stringsAsFactors = FALSE)
+    res$company <- symbol
+    output <- rbind(output, res)
+  }
+  output <- output%>%
+    mutate(date = lubridate::ymd(Date)) %>%
+    select(-Date)
+  if (yahooCode == 'v') {
+    output <-
+      output %>%
+      rename(dividends = Dividends)
+  } else {
+    output <-
+      output %>%
+      rename(open = Open, high=High, low=Low, close=Close,
+             volume = Volume, adj_close = Adj.Close)
+  }
+
+  output
+}
